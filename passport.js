cbe3c39bb88196eefe5258f55afae743a7fdcbf3
Michael Chen <michaelchengaming@gmail.com>
2020-05-22 19:32:26 -0700

    add more middleware stuff to passport for authorizing and authenticating users

--- passport.js
+++ passport.js
@@ -1,8 +1,38 @@
+import "dotenv/config";
 import passport from "passport";
 const LocalStategy = require("passport-local").Strategy;
+const JwtStrategy = require("passport-jwt").Strategy;
 
 import User from "./src/models/user";
 
+// extracts the jwt from the cookie
+const cookieExtractor = (req) => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies["access_token"];
+  }
+  return token;
+};
+
+// middleware used for authorization - protecting a resource
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: cookieExtractor,
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    (payload, done) => {
+      User.findById({ _id: payload.sub }, (error, user) => {
+        if (error) return done(error, false);
+        // if user found, no error, and pass user
+        if (user) return done(null, user);
+        else return done(null, false);
+      });
+    }
+  )
+);
+
+// middleware for auth local strat using username and password
 passport.use(
   new LocalStategy((username, password, done) => {
     User.findOne({ username: username }, (error, user) => {
